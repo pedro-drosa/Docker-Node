9e1ca0faad75b57d35af435634f3223ca8fda64d
pedro-drosa <pedro_drosa@hotmail.com>
2022-03-10 20:01:28 -0300

    build: add dotenv

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
+import dotenv from "dotenv";
 import express from "express";
 import routes from "./routes/index.routes.js";
 
+dotenv.config();
+
 const app = express();
+
 app.use(express.json());
 app.use(routes);
 
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import App from "./app.js";
-const port = 5000;
+
+const host = process.env.API_HOST;
+const port = process.env.API_PORT;
 
 App.listen(port, () => {
-  console.log(`⚡ Server listen on port:${port}`);
+  console.log(`⚡ Server running on ${host}:${port}`);
 });
