d89879b0ea8a667b49b93978172b98fe55738a5a
pedro-drosa <pedro_drosa@hotmail.com>
2022-03-11 09:22:27 -0300

    feat(User): defining user schema

---
+++ src/models/User.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const UserSchema = new Schema(
+  {
+    name: String,
+    email: String,
+    password: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("Users", UserSchema);
+export default User;
