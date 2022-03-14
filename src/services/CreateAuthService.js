5260a248651dd9e6d057fdafc4865240c7b666e3
pedro-drosa <pedro_drosa@hotmail.com>
2022-03-14 17:21:56 -0300

    feat(CreateAuthService): generating the authentication token

--- src/services/CreateAuthService.js
+++ src/services/CreateAuthService.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import UserRepository from '../repositories/UserRepository.js';
 import PasswordHash from '../utils/PasswordHash.js';
 
@@ -5,6 +6,9 @@ const userRepository = new UserRepository();
 
 class CreateAuthService {
   static async execute({ name, email, password }) {
+    const secretKey = process.env.JWT_SECRET_KEY;
+    const expiresIn = process.env.JWT_EXPIRES_IN;
+
     const userExists = await userRepository.findOneUserByNameOrEmail(
       name,
       email
@@ -17,7 +21,13 @@ class CreateAuthService {
       );
 
       if (passwordIsCorrect) {
-        return userExists;
+        const token = jwt.sign({ id: userExists.id }, secretKey, {
+          expiresIn,
+        });
+
+        const { _id, name, email, createdAt } = userExists;
+
+        return { _id, name, email, createdAt, token };
       }
     }
 
